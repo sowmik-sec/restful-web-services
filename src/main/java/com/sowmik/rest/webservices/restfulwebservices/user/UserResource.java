0252eaa510b060c7e38520e90f68f20cf834d088
sowmik-sec <sowmik.sec@gmail.com>
2023-11-25 13:14:29 +0600

    implement validations for rest api

--- src/main/java/com/sowmik/rest/webservices/restfulwebservices/user/UserResource.java
+++ src/main/java/com/sowmik/rest/webservices/restfulwebservices/user/UserResource.java
@@ -1,5 +1,6 @@
 package com.sowmik.rest.webservices.restfulwebservices.user;
 
+import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -40,7 +41,7 @@ public class UserResource {
 
     //POST /users
     @PostMapping("/users")
-    public ResponseEntity<User> createUser(@RequestBody User user) {
+    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
         User savedUser = service.save(user);
         // /users/2 => /users/{id}, user.getId
         URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(savedUser.getId()).toUri();
