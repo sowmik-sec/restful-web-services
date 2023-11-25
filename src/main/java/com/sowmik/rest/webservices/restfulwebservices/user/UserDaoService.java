7f9ceb5fca290204a035980c52e027f1245f2b15
sowmik-sec <sowmik.sec@gmail.com>
2023-11-25 10:57:13 +0600

    implement get methods for user resource

--- src/main/java/com/sowmik/rest/webservices/restfulwebservices/user/UserDaoService.java
+++ src/main/java/com/sowmik/rest/webservices/restfulwebservices/user/UserDaoService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Component;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Predicate;
 
 @Component
 public class UserDaoService {
@@ -14,11 +15,15 @@ public class UserDaoService {
     static {
         users.add(new User(1,"Adam", LocalDate.now().minusYears(30)));
         users.add(new User(2,"Eve", LocalDate.now().minusYears(25)));
-        users.add(new User(1,"David", LocalDate.now().minusYears(22)));
+        users.add(new User(3,"David", LocalDate.now().minusYears(22)));
     }
     public List<User> findAll(){
         return users;
     }
     //public User save(User user){}
-    //public User findOne(int id){}
+    public User findOne(int id){
+        Predicate<? super User> predicate = user -> user.getId().equals(id);
+        return users.stream().filter(predicate).findFirst().get();
+
+    }
 }
