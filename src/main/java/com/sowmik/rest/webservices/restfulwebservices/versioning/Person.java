ece80b21de59bb9f7a5ea2dd71481b7c083b2120
sowmik-sec <sowmik.sec@gmail.com>
2023-11-26 12:09:39 +0600

    versioning rest api - uri versioning

---
+++ src/main/java/com/sowmik/rest/webservices/restfulwebservices/versioning/Person.java
@@ -0,0 +1,8 @@
+package com.sowmik.rest.webservices.restfulwebservices.versioning;
+
+public class Person {
+    String name;
+    public Person(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/com/sowmik/rest/webservices/restfulwebservices/versioning/VersioningPersonController.java
@@ -0,0 +1,16 @@
+package com.sowmik.rest.webservices.restfulwebservices.versioning;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersioningPersonController {
+    @GetMapping("/v1/person")
+    public PersonV1 getFirstVersionOfPerson() {
+        return new PersonV1("Roman Reigns");
+    }
+    @GetMapping("/v2/person")
+    public PersonV2 getSecondVersionOfPerson() {
+        return new PersonV2(new Name("Roman", "Reigns"));
+    }
+}
