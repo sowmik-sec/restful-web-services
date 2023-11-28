d0d30d990ef88f5787bdd8924642f13fdf05cc43
sowmik-sec <sowmik.sec@gmail.com>
2023-11-28 10:43:07 +0600

    implement static filtering for rest api

---
+++ src/main/java/com/sowmik/rest/webservices/restfulwebservices/filtering/SomeBean.java
@@ -0,0 +1,41 @@
+package com.sowmik.rest.webservices.restfulwebservices.filtering;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+//properties can be ignored in different ways.
+
+@JsonIgnoreProperties("field1")
+public class SomeBean {
+    private String field1;
+    @JsonIgnore
+    private String field2;
+    private String field3;
+
+    public SomeBean(String field1, String field2, String field3) {
+        this.field1 = field1;
+        this.field2 = field2;
+        this.field3 = field3;
+    }
+
+    public String getField1() {
+        return field1;
+    }
+
+    public String getField2() {
+        return field2;
+    }
+
+    public String getField3() {
+        return field3;
+    }
+
+    @Override
+    public String toString() {
+        return "SomeBean{" +
+                "field1='" + field1 + '\'' +
+                ", field2='" + field2 + '\'' +
+                ", field3='" + field3 + '\'' +
+                '}';
+    }
+}
