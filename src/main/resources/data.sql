7404f6a8cf4236f48d23736e30c873565fc52e41
sowmik-sec <sowmik.sec@gmail.com>
2023-11-30 11:14:48 +0600

    create user entity and some test data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into user_details(id, birth_date, name)
+values (1001, current_date(), 'Ahsan');
+insert into user_details(id, birth_date, name)
+values (1002, current_date(), 'Habib');
+insert into user_details(id, birth_date, name)
+values (1003, current_date(), 'Sowmik');
+insert into user_details(id, birth_date, name)
+values (1004, current_date(), 'Volga');
