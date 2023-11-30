ada56bff90ef6549849421246882a16f5fe8e623
sowmik-sec <sowmik.sec@gmail.com>
2023-11-30 12:38:26 +0600

    create post entity with many to one relationship with user entity

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,3 +6,12 @@ insert into user_details(id, birth_date, name)
 values (1003, current_date(), 'Sowmik');
 insert into user_details(id, birth_date, name)
 values (1004, current_date(), 'Volga');
+
+insert into post(id, description, user_id)
+values (2001, 'I want to learn AWS', 1001);
+insert into post(id, description, user_id)
+values (2002, 'I want to learn DevOps', 1001);
+insert into post(id, description, user_id)
+values (2003, 'I want to learn Backend', 1002);
+insert into post(id, description, user_id)
+values (2004, 'I want to learn Frontend', 1002);
